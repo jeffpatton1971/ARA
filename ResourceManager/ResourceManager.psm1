bf4ce4312a2e1a225e69c95312a3ebd136f8940e
Jeff Patton <jeffrey@patton-tech.com>
2016-06-17 16:19:21 -0500

    Checking in a new cmdlet that will pull the health history of a given resource

--- ResourceManager/ResourceManager.psm1
+++ ResourceManager/ResourceManager.psm1
@@ -1,9 +1,12 @@
+$RestAPIVersion = "2015-01-01";
+
 function Get-AzureResourceProvider
 {
 	param
 	(
 		[string]$SubscriptionId = $Global:AzureSubscription.subscriptionId,
-		[string]$Namespace
+		[string]$Namespace,
+		[string]$apiVersion
 	)
 
 	try
@@ -12,7 +15,10 @@ function Get-AzureResourceProvider
 		$Error.Clear();
 
 		$Method = 'GET';
-		$apiVersion = '2015-01-01';
+		if (!($apiVersion))
+		{
+			$apiVersion = $RestAPIVersion;
+		}
 
 		$Uri = "https://management.azure.com/subscriptions/$($SubscriptionId)/providers";
 
@@ -22,6 +28,38 @@ function Get-AzureResourceProvider
 		}
 
 		$Uri = "$($Uri)?api-version=$($apiVersion)";
+		
+		Invoke-AzureRestAPI -Method $Method -apiVersion $apiVersion -AuthenticationResult $Global:AzureAuthenticationResult -Uri $Uri;
+	}
+	catch
+	{
+		throw $Error;
+	}
+}
+function Get-AzureResourceHealth
+{
+	param
+	(
+		[string]$SubscriptionId = $Global:AzureSubscription.subscriptionId,
+		[ValidateSet('Microsoft.Compute','Microsoft.Network','Microsoft.Storage')] 
+		[string]$Provider,
+		[string]$ResourceGroupName,
+		[string]$ResourceType,
+		[string]$ResourceName,
+		[string]$apiVersion
+	)
+
+	try
+	{
+		$ErrorActionPreference = 'Stop';
+		$Error.Clear();
+
+		$Method = 'GET';
+		if (!($apiVersion))
+		{
+			$apiVersion = $RestAPIVersion;
+		}
+		$Uri = "https://management.azure.com/subscriptions/$($SubscriptionId)/resourceGroups/$($ResourceGroupName)/providers/$($Provider)/$($ResourceType)/$($ResourceName)/providers/Microsoft.ResourceHealth/availabilityStatuses?api-version=$($apiVersion)";
 		
 		Invoke-AzureRestAPI -Method $Method -apiVersion $apiVersion -AuthenticationResult $Global:AzureAuthenticationResult -Uri $Uri;
 	}
