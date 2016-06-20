42cee778877ea554e46e60ec10fabaf7382d277f
Jeff Patton <jeffrey@patton-tech.com>
2016-06-20 16:59:57 -0500

    This is a pile of code around creating proper json for new alerts. I'm hoping that this is generic enough that i could pull it out into a seperate module and reference from anywhere, but for the time being the new-jsonalert and cmldets below are specific for azure alerts. Additionally checking in code that uses those to create an alert in azure against a given resource, the code mostly works there are datatype issues and property issues i'm working through but the code generated is valid and if the types and such were valid would yield an alert. As it stands now, we get a proper error message back about invalid namespaces for metrics.

--- Insights/Insights.psm1
+++ Insights/Insights.psm1
@@ -143,3 +143,180 @@ function Get-AzureAlertRule
 		throw $Error;
 	}
 }
+
+function New-AzureAlert
+{
+	param
+	(
+		[parameter(Mandatory=$true)]
+		[string]$ResourceGroupName,
+		[parameter(Mandatory=$true)]
+		[string]$Location,
+		[parameter(Mandatory=$false)]
+		[hashtable[]]$Tags,
+		[parameter(Mandatory=$true)]
+		[string]$Name,
+		[parameter(Mandatory=$false)]
+		[string]$Description,
+		[parameter(Mandatory=$true)]
+		[boolean]$IsEnabled,
+		[parameter(Mandatory=$true)]
+		[string]$ResourceUri,
+		[parameter(Mandatory=$true)]
+		[string]$MetricName,
+		[parameter(Mandatory=$true)]
+		[int]$Threshold,
+		[parameter(Mandatory=$true)]
+		[int]$WindowSize,
+		[parameter(Mandatory=$true)]
+		[validateset('average','count','last','minimum','maximum','total')]
+		[string]$TimeAggregation,
+		[parameter(Mandatory=$true)]
+		[boolean]$SendToServiceOwners,
+		[parameter(Mandatory=$false)]
+		[string[]]$CustomEmails,
+		[parameter(Mandatory=$false)]
+		[string]$SubscriptionId = $Global:AzureSubscription.subscriptionId,
+		[parameter(Mandatory=$false)]
+		[string]$apiVersion,
+		[parameter(Mandatory=$false)]
+		[switch]$AsJson
+	)
+
+	try
+	{
+		$ErrorActionPreference = 'Stop';
+		$Error.Clear();
+
+		$Method = 'PUT';
+		if (!($apiVersion))
+		{
+			$apiVersion = $RestAPIVersion;
+		}
+
+		$Uri = "https://management.azure.com/subscriptions/$($SubscriptionId)/resourceGroups/$($ResourceGroupName)/providers/microsoft.insights/alertRules/$($Name)?api-version=$($apiVersion)";
+		$PayLoad = New-JsonAlert -Location $Location -Tags $Tags -Name $Name -Description $Description -IsEnabled $IsEnabled -ResourceUri $ResourceUri -MetricName $MetricName -Threshold $Threshold -WindowSize $WindowSize -TimeAggregation $TimeAggregation -SendToServiceOwners $SendToServiceOwners -CustomEmails $CustomEmails |ConvertTo-Json -Depth 4;
+
+		if ($AsJson)
+		{
+			Invoke-AzureRestAPI -Method $Method -apiVersion $apiVersion -AuthenticationResult $Global:AzureAuthenticationResult -Uri $Uri -Payload $Payload |ConvertTo-Json;
+		}
+		else
+		{
+			Invoke-AzureRestAPI -Method $Method -apiVersion $apiVersion -AuthenticationResult $Global:AzureAuthenticationResult -Uri $Uri -Payload $Payload;
+		}
+	}
+	catch
+	{
+		throw $Error;
+	}
+}
+
+function New-JsonAlert
+{
+	param
+	(
+		[parameter(Mandatory=$true)]
+		[string]$Location,
+		[parameter(Mandatory=$false)]
+		[hashtable[]]$Tags,
+		[parameter(Mandatory=$true)]
+		[string]$Name,
+		[parameter(Mandatory=$false)]
+		[string]$Description,
+		[parameter(Mandatory=$true)]
+		[boolean]$IsEnabled,
+		[parameter(Mandatory=$true)]
+		[string]$ResourceUri,
+		[parameter(Mandatory=$true)]
+		[string]$MetricName,
+		[parameter(Mandatory=$true)]
+		[int]$Threshold,
+		[parameter(Mandatory=$true)]
+		[int]$WindowSize,
+		[parameter(Mandatory=$true)]
+		[validateset('average','count','last','minimum','maximum','total')]
+		[string]$TimeAggregation,
+		[parameter(Mandatory=$true)]
+		[boolean]$SendToServiceOwners,
+		[parameter(Mandatory=$false)]
+		[string[]]$CustomEmails
+	)
+
+	$DataSource = New-DataSource -ResourceUri $ResourceUri -MetricName $MetricName
+	$Condition = New-Condition -Datasource $DataSource -Threshold $Threshold -WindowSize $WindowSize -TimeAggregation $TimeAggregation 
+	$Action = New-Action -SendToServiceOwners $SendToServiceOwners -CustomEmails $CustomEmails
+
+	$Properties = New-Object -TypeName psobject -Property @{
+		'name' = $Name
+		'description' = $Description
+		'isEnabled' = $IsEnabled
+		'condition' = $Condition
+		'action' = $Action
+	} |Select-Object -Property name, description, isEnabled, condition, action
+	New-Object -TypeName psobject -Property @{
+		'location' = $Location
+		'tags' = $Tags
+		'properties' = $Properties
+	} |Select-Object -Property location, tags, properties
+}
+
+function New-DataSource
+{
+	param
+	(
+		[parameter(Mandatory=$false)]
+		[string]$Type = 'Microsoft.Azure.Management.Insights.Models.RuleMetricDataSource',
+		[parameter(Mandatory=$true)]
+		[string]$ResourceUri,
+		[parameter(Mandatory=$true)]
+		[string]$MetricName
+	)
+	New-Object -TypeName psobject -Property @{
+		'odata.type' = $Type
+		'resourceUri' = $ResourceUri
+		'metricName' = $TimeAggregation
+	} |Select-Object -Property 'odata.type','resourceUri','metricName'
+}
+function New-Condition
+{
+	param
+	(
+		[parameter(Mandatory=$false)]
+		[string]$Type = 'Microsoft.Azure.Management.Insights.Models.ThresholdRuleCondition',
+		[parameter(Mandatory=$true)]
+		[object]$Datasource,
+		[parameter(Mandatory=$true)]
+		[int]$Threshold,
+		[parameter(Mandatory=$true)]
+		[validateset(5,10,15,30,45,60)]
+		[int]$WindowSize,
+		[parameter(Mandatory=$true)]
+		[validateset('average','count','last','minimum','maximum','total')]
+		[string]$TimeAggregation
+	)
+	New-Object -TypeName psobject -Property @{
+		'odata.type' = $Type
+		'datasource' = $DataSource
+		'threshold' = $Threshold
+		'windowSize' = "PT$($WindowSize)M"
+		'timeAggregation' = $TimeAggregation
+	} |Select-Object -Property 'odata.type','datasource','threshold','windowSize','timeAggregation'
+}
+function New-Action
+{
+	param
+	(
+		[parameter(Mandatory=$false)]
+		[string]$Type = 'Microsoft.Azure.Management.Insights.Models.RuleEmailAction',
+		[parameter(Mandatory=$true)]
+		[boolean]$SendToServiceOwners,
+		[parameter(Mandatory=$false)]
+		[string[]]$CustomEmails
+	)
+	New-Object -TypeName psobject -Property @{
+		'odata.type' = $Type
+		'sendToServiceOwners' = $SendToServiceOwners
+		'customEmails' = $CustomEmails
+	} |Select-Object -Property 'odata.type','sendToServiceOwners','customEmails'
+}
