08eefa7947b140e6029f4cbf4e11e7f443495fb6
Jeff Patton <jeffrey@patton-tech.com>
2017-07-18 10:48:37 -0500

    Stubbing in code from https://gallery.technet.microsoft.com/scriptcenter/Easily-obtain-AccessToken-3ba6e593

--- Authentication/Authentication.psm1
+++ Authentication/Authentication.psm1
@@ -93,3 +93,31 @@ function Select-AzureSubscription
 		throw $Error;
 	}
 }
+function Get-AzureRmCachedAccessToken()
+{
+  $ErrorActionPreference = 'Stop'
+  
+  if(-not (Get-Module AzureRm.Profile)) {
+    Import-Module AzureRm.Profile
+  }
+  $azureRmProfileModuleVersion = (Get-Module AzureRm.Profile).Version
+  # refactoring performed in AzureRm.Profile v3.0 or later
+  if($azureRmProfileModuleVersion.Major -ge 3) {
+    $azureRmProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    if(-not $azureRmProfile.Accounts.Count) {
+      Write-Error "Ensure you have logged in before calling this function."    
+    }
+  } else {
+    # AzureRm.Profile < v3.0
+    $azureRmProfile = [Microsoft.WindowsAzure.Commands.Common.AzureRmProfileProvider]::Instance.Profile
+    if(-not $azureRmProfile.Context.Account.Count) {
+      Write-Error "Ensure you have logged in before calling this function."    
+    }
+  }
+  
+  $currentAzureContext = Get-AzureRmContext
+  $profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azureRmProfile)
+  Write-Debug ("Getting access token for tenant" + $currentAzureContext.Subscription.TenantId)
+  $token = $profileClient.AcquireAccessToken($currentAzureContext.Subscription.TenantId)
+  $token.AccessToken
+}
